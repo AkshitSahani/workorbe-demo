b368df1b1275ba683cdd1d9e7c4ac5b399af9cce
Akshit Sahani <akshit_sahani@hotmail.com>
2018-02-01 14:33:07 -0500

    tie signup and login flow back to search results screen

--- src/components/actionCreators.js
+++ src/components/actionCreators.js
@@ -1,5 +1,7 @@
+import fireBase from 'firebase';
 import firebase from '@firebase/app';
 import '@firebase/firestore';
+import {Actions} from 'react-native-router-flux';
 
 export const loginSuccess = (user) => {return {type: 'loginSuccess', user: user}};
 
@@ -16,8 +18,8 @@ export const signUpFailure = (error) => {return {type: 'signUpFailure', error: e
 export const login = (email, password) => {
   return (dispatch) => {
     dispatch(loginStart());
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((user) => { return dispatch(loginSuccess(user))})
+    fireBase.auth().signInWithEmailAndPassword(email, password)
+      .then((user) => {dispatch(loginSuccess(user)); Actions.SearchResults();})
       .catch((error)=>{return dispatch(loginFailure(error))});
   };
 };
@@ -25,8 +27,8 @@ export const login = (email, password) => {
 export const signUp = (email, password) => {
   return (dispatch) => {
     dispatch(signUpStart());
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then((user)=>{return dispatch(signUpSuccess(user))})
+    fireBase.auth().createUserWithEmailAndPassword(email, password)
+      .then((user)=>{dispatch(signUpSuccess(user)); Actions.SearchResults();})
       .catch((error)=>{return dispatch(signUpFailure(error))})
   };
 };
